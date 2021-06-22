7105104c6e5710c487f860a0112e94f3429a0a49
Edson Silva <edson@purespectrum.com>
2021-06-22 17:22:25 -0300

    create enum pokemart

--- src/project/pokemonmaster/PokeMart.java
+++ src/project/pokemonmaster/PokeMart.java
@@ -6,26 +6,24 @@ import java.util.HashMap;
 //TODO: Implements an ENUM items (OK)
 //TODO: Implements a BUY method ()
 //TODO: Implements a SELL method ()
-public class PokeMart {
+public enum PokeMart {
+
+    ANTIDOTE_CURE_POISONING(),
+    AWAKENING_CURE_SLEEP,
+    BURN_HEAL_CURE_BURNING,
+    PARALIZE_HEAL_CURE_PARALIZE,
+    ETHER,
+    POTION_20_HP,
+    SUPER_POTION_60_HP,
+    HIPER_POTION_120_HP,
+    MAX_POTION_ALL_HP,
+    REVIVE_20_AP,
+    MAX_REVIVE_ALL_HP;
 
     private ArrayList itemList = new ArrayList();
     private HashMap item = new HashMap<String, Float>();
 
-    private enum ITEMS {
-        ANTIDOTE_CURE_POISONING,
-        AWAKENING_CURE_SLEEP,
-        BURN_HEAL_CURE_BURNING,
-        PARALIZE_HEAL_CURE_PARALIZE,
-        ETHER,
-        POTION_20_HP,
-        SUPER_POTION_60_HP,
-        HIPER_POTION_120_HP,
-        MAX_POTION_ALL_HP,
-        REVIVE_20_AP,
-        MAX_REVIVE_ALL_HP
-    }
-
-    public void buyItem () {
+    public void buyItem (Enum item, int amount) {
 
     }
 }
